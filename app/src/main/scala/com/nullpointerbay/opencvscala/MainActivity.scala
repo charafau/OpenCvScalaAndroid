9eb39d4b9f16dae0f90c7bd57d18d3e300c23b34
charafau <gorudonu@gmail.com>
2016-08-20 03:54:33 +0900

    fliped image on 90 degrees

--- app/src/main/scala/com/nullpointerbay/opencvscala/MainActivity.scala
+++ app/src/main/scala/com/nullpointerbay/opencvscala/MainActivity.scala
@@ -6,11 +6,18 @@ import org.opencv.android._
 import android.util.Log
 import android.view.View
 import org.opencv.android.CameraBridgeViewBase.{CvCameraViewFrame, CvCameraViewListener2}
+import org.opencv.core.{Core, CvType, Mat}
+import org.opencv.imgproc.Imgproc
 
 class MainActivity extends Activity with TypedFindView
   with CvCameraViewListener2 {
   lazy val textview = findView(TR.text)
   lazy val openCvCameraView = findViewById(R.id.HelloOpenCvView).asInstanceOf[CameraBridgeViewBase]
+
+  var rgba: Mat = _
+  var rgbaF: Mat = _
+  var rgbaT: Mat = _
+
 
   val TAG = "MainActivity"
   val loaderCallback = new BaseLoaderCallback(this) {
@@ -33,7 +40,6 @@ class MainActivity extends Activity with TypedFindView
     textview.setText("Hello world, from opencvscala")
     openCvCameraView.setVisibility(View.VISIBLE);
     openCvCameraView.setCvCameraViewListener(this);
-    openCvCameraView.setRotation(90f)
 
   }
 
@@ -63,26 +69,21 @@ class MainActivity extends Activity with TypedFindView
     }
   }
 
-  override def onCameraViewStarted(width: Int, height: Int) = {}
+  override def onCameraViewStarted(width: Int, height: Int) = {
+    rgba = new Mat(height, width, CvType.CV_8UC4)
+    rgbaF = new Mat(height, width, CvType.CV_8UC4)
+    rgbaT = new Mat(width, width, CvType.CV_8UC4)
+  }
 
   override def onCameraViewStopped() = {}
 
-  override def onCameraFrame(inputFrame: CvCameraViewFrame) = inputFrame.rgba()
+  override def onCameraFrame(inputFrame: CvCameraViewFrame) = {
+    rgba = inputFrame.rgba()
+    Core.transpose(rgba, rgbaT)
+    Imgproc.resize(rgbaT, rgbaF, rgbaF.size(), 0, 0, 0)
+    Core.flip(rgbaF, rgba, 1)
 
-}
-
-object MainActivity {
-
-  val TAG = "MainActivity"
-
-  def load(): Unit = {
-    //
-    //    if (!OpenCVLoader.initDebug()) {
-    //      Log.d(TAG, "OpenCv Not Loaded")
-    //    } else {
-    //      Log.d(TAG, "OpenCv Loaded")
-    //    }
-
+    rgba
   }
 
 }
