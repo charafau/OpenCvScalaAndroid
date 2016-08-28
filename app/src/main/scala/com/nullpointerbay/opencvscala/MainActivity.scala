b1482f07a349f4f6155599f405d41e268c7dbf23
charafau <gorudonu@gmail.com>
2016-08-29 01:18:03 +0900

    Cleaned dependencies Upgraded sbt android plugin

--- app/src/main/scala/com/nullpointerbay/opencvscala/MainActivity.scala
+++ app/src/main/scala/com/nullpointerbay/opencvscala/MainActivity.scala
@@ -2,22 +2,17 @@ package com.nullpointerbay.opencvscala
 
 import android.app.Activity
 import android.os.Bundle
-import org.opencv.android._
 import android.util.Log
 import android.view.View
 import org.opencv.android.CameraBridgeViewBase.{CvCameraViewFrame, CvCameraViewListener2}
-import org.opencv.core.{Core, CvType, Mat}
-import org.opencv.imgproc.Imgproc
+import org.opencv.android._
+import org.opencv.core.{CvType, Mat}
 
 class MainActivity extends Activity with TypedFindView
   with CvCameraViewListener2 {
-  lazy val textview = findView(TR.text)
   lazy val openCvCameraView = findViewById(R.id.HelloOpenCvView).asInstanceOf[CameraBridgeViewBase]
 
   var rgba: Mat = _
-  var rgbaF: Mat = _
-  var rgbaT: Mat = _
-
 
   val TAG = "MainActivity"
   val loaderCallback = new BaseLoaderCallback(this) {
@@ -37,7 +32,6 @@ class MainActivity extends Activity with TypedFindView
   override def onCreate(savedInstanceState: Bundle): Unit = {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
-    textview.setText("Hello world, from opencvscala")
     openCvCameraView.setVisibility(View.VISIBLE);
     openCvCameraView.setCvCameraViewListener(this);
 
@@ -71,17 +65,12 @@ class MainActivity extends Activity with TypedFindView
 
   override def onCameraViewStarted(width: Int, height: Int) = {
     rgba = new Mat(height, width, CvType.CV_8UC4)
-    rgbaF = new Mat(height, width, CvType.CV_8UC4)
-    rgbaT = new Mat(width, width, CvType.CV_8UC4)
   }
 
   override def onCameraViewStopped() = {}
 
   override def onCameraFrame(inputFrame: CvCameraViewFrame) = {
     rgba = inputFrame.rgba()
-    Core.transpose(rgba, rgbaT)
-    Imgproc.resize(rgbaT, rgbaF, rgbaF.size(), 0, 0, 0)
-    Core.flip(rgbaF, rgba, 1)
 
     rgba
   }
